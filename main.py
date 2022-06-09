842e491227232ce2d6f90337f7ad89c8d38ca872
Simon Gomola <73365650+simdesu@users.noreply.github.com>
2022-06-09 21:25:25 +0200

    work further with the first result

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 while True:
     try:
-        num1 = int(input("cislo jedna: "))
-        num2 = int(input("cislo dva: "))
+        num1 = int(input("first number: "))
+        num2 = int(input("second number: "))
     except ValueError:
-        print("napis cislo normalne ty kokot")
+        print("numbers only, try again")
         continue
     
     while True:
-        znak = str(input("znak: "))
+        znak = str(input("symbol: "))
         if znak in ('+', '-', '*', '/'):
             break
-        print("len tieto znaky pouzivaj: +; -; *; /")
+        print("only use the following symbols: +; -; *; /")
     else:
         continue
 
@@ -30,25 +30,37 @@ while True:
                 vysledok = int(num1 / num2)
                 print(num1, "/", num2, "sa rovna", vysledok)
 
-            opakovanie = str(input('chces s vysledkom este pracovat (y/n)?'))
+            opakovanie = str(input('do you wanna work with this result (y/n)?'))
             if opakovanie in ('y', 'n'):
                 break
             print("invalid input.")
     if opakovanie == 'y':
         try:
-            num2 = int(input("cislo dva: "))
+            num2 = int(input("second number: "))
         except ValueError:
-            print("napis cislo normalne ty kokot")
+            print("numbers only, try again")
             continue
         while True:
-                znak = str(input("znak: "))
-        
-    if znak in ('+', '-', '*', '/'):
-        break
-    print("len tieto znaky pouzivaj: +; -; *; /")
-    
-    # elif opakovanie == 'n':
-    #    break
+                znak = str(input("symbol: "))
+                if znak in ('+', '-', '*', '/'):
+                    break
+                print("only use the following symbols: +; -; *; /")
+            
+        if (znak == "+"):
+            vysledok = int(vysledok + num2)
+            print(num1, "+", num2, "sa rovna", vysledok)
+        elif (znak == "-"):
+            vysledok = int(vysledok - num2)
+            print(num1, "-", num2, "sa rovna", vysledok)
+        elif (znak == "*"):
+            vysledok = int(vysledok * num2)
+            print(num1, "*", num2, "sa rovna", vysledok)
+        elif (znak == "/"):
+            vysledok = int(vysledok / num2)
+            print(num1, "/", num2, "sa rovna", vysledok)
+
+    elif opakovanie == 'n':
+       break
     
 
     while True:
